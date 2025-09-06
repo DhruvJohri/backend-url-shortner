7bbf450219b21a64faf2744ee073fdffd41131b0
Dhruv Johri <111191087+DhruvJohri@users.noreply.github.com>
2025-09-06 11:24:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.config("./.env")
 const app = express();
 
 app.use(cors({
-    origin: 'https://frontend-url-shortner.vercel.app/', // your React app
+    origin: 'https://frontend-url-shortner-git-main-dhruv-johris-projects.vercel.app/', // your React app
     credentials: true // 👈 this allows cookies to be sent
 }));
 
@@ -39,4 +39,5 @@ app.listen(3000,()=>{
 })
 
 // GET - Redirection 
+
 
